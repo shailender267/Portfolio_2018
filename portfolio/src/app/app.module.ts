ea21227790b65a69f377577551e5c639e102fe92
Devi Shailender Singh <shailender267@gmail.com>
2018-02-19 11:34:27 -0500

    To Prateek WebPack Issue
    To Prateek WebPack Issue

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import 'bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ProjectPageComponent,
+    AboutPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule
